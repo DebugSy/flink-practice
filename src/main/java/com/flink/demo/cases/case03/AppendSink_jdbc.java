3a0986aa78c7de693e0361a175c5677b44cc0b40
shiyong <yong.shi@inforefiner.com>
2019-07-29 20:06:47 +0800

    append jdbc sink

---
+++ src/main/java/com/flink/demo/cases/case03/AppendSink_jdbc.java
@@ -0,0 +1,53 @@
+package com.flink.demo.cases.case03;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.table.sinks.AppendStreamTableSink;
+import org.apache.flink.table.sinks.TableSink;
+import org.apache.flink.types.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by DebugSy on 2019/7/29.
+ */
+public class AppendSink_jdbc implements AppendStreamTableSink<Row>{
+
+    private static final Logger logger = LoggerFactory.getLogger(AppendSink_jdbc.class);
+
+    private String[] fieldNames;
+
+    private TypeInformation<?>[] fieldTypes;
+
+    public AppendSink_jdbc(String[] fieldNames, TypeInformation<?>[] fieldTypes) {
+        this.fieldNames = fieldNames;
+        this.fieldTypes = fieldTypes;
+    }
+
+    @Override
+    public void emitDataStream(DataStream<Row> dataStream) {
+        logger.info("Print append jdbc sink.");
+        dataStream.print();
+    }
+
+    @Override
+    public TypeInformation<Row> getOutputType() {
+        return new RowTypeInfo(fieldTypes, fieldNames);
+    }
+
+    @Override
+    public String[] getFieldNames() {
+        return fieldNames;
+    }
+
+    @Override
+    public TypeInformation<?>[] getFieldTypes() {
+        return fieldTypes;
+    }
+
+    @Override
+    public TableSink<Row> configure(String[] fieldNames, TypeInformation<?>[] fieldTypes) {
+        return new AppendSink_jdbc(fieldNames, fieldTypes);
+    }
+}
---
+++ src/main/java/com/flink/demo/cases/case03/FlinkSinkTraining_jdbc.java
@@ -0,0 +1,63 @@
+package com.flink.demo.cases.case03;
+
+import com.flink.demo.cases.common.datasource.UserDataSource;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.sinks.CsvTableSink;
+import org.apache.flink.types.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by DebugSy on 2019/7/29.
+ */
+public class FlinkSinkTraining_jdbc {
+
+    private static final Logger logger = LoggerFactory.getLogger(FlinkSinkTraining_jdbc.class);
+
+    private static String userFields = "userId,username,address,activityTime";
+
+    private static String[] fieldNames = {"userId", "username", "address", "activityTime"};
+
+    private static TypeInformation[] typeInfos = {
+            TypeInformation.of(Integer.class),
+            TypeInformation.of(String.class),
+            TypeInformation.of(String.class),
+            TypeInformation.of(Timestamp.class)
+    };
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
+
+        StreamTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
+
+        DataStreamSource<Tuple4<Integer, String, String, Timestamp>> userStream = env.addSource(new UserDataSource());
+        tableEnv.registerDataStream("users", userStream, userFields);
+        tableEnv.registerTableSink("jdb1c_sink", new AppendSink_jdbc(fieldNames, typeInfos));
+
+        tableEnv.sqlUpdate("insert into jdb1c_sink SELECT * FROM users");
+
+//        DataStream<Row> sinkStream = tableEnv.toAppendStream(table, Row.class);
+//        sinkStream.addSink(new SinkFunction<Row>() {
+//            @Override
+//            public void invoke(Row value, Context context) throws Exception {
+//                logger.info("print {}", value);
+//            }
+//        });
+
+        env.execute("Flink sql sink training for jdbc");
+    }
+
+}
